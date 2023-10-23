fcc34a4a0ef41d45fe883a3b95261de96289c161
Chetan Kumar <kumarchetannoorpur2000@gmail.com>
2023-10-23 18:42:07 +0000

    Functions for array and objects

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -19,13 +19,13 @@ function printMyName(){
 // addTwoNumber(1, "C") // 1C
 // addTwoNumber(1, 8) // 9
 
-function addTwoNumber(num1, num2){
+function addTwoNumber(num1, num2){ // Here num1 and num2 called Parameters
     let result = num1+num2
     return result;
     // console.log("Chetan"); // Any value after return statement not get executed
 }
 
-let result = addTwoNumber(4,9)
+let result = addTwoNumber(4,9) // Here 4, 9 are called Arguments
 // console.log("Result is : " + result);
 
 
@@ -42,4 +42,44 @@ function loginMessage(userName = "Sam"){
     return `${userName} just logged in!`
  }
 
- loginMessage("Chetan")
+//  console.log(loginMessage("Chetan"));
+
+function shoppingCartPrice(...num) { // Here ... => is used as Rest operator, sometimes it is used as spread operator as we have used it in object and array merging
+
+    return num
+    
+}
+
+console.log(shoppingCartPrice(200, 400, 900)); // this returns [ 200, 400, 900 ] array
+
+function shoppingCartPrice2(item1, item2, ...item) { // Here ... => is used as Rest operator, sometimes it is used as spread operator as we have used it in object and array merging
+
+    return item
+}
+
+console.log(shoppingCartPrice2(200, 400, 900, 2999)); // returns because first two arguments are assigned to first two parameters [ 900, 2999 ]
+
+
+// Passing Arrays and Objects in the function
+
+let loginUser = {
+    name: "Sam",
+    age: 23
+}
+
+function showObject(yourObject){
+    return `User name is ${yourObject.name} and age is ${yourObject.age}`
+}
+
+console.log(showObject(loginUser));
+
+
+// Array
+
+let myNewArray = [2, 4, 1, 5, 6]
+
+function returnArrayValues(yourArray) {
+    return yourArray[0]
+}
+
+console.log(returnArrayValues(myNewArray));
