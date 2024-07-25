f1b813f5252d6c693386d05cf905f4a86fbad690
Chetan Kumar <kumarchetannoorpur2000@gmail.com>
2024-07-25 21:02:46 +0000

    while and do while loop

---
+++ 05_iterators/02_while-dowhile.js
@@ -0,0 +1,22 @@
+
+let num = 1
+while (num<=10) {
+    // console.log(`Counting number is ${num}`)
+    num++;
+}
+
+let NamesOfGod = ["Krishna", "Vishnu", "Gopal", "Nandlaal", "Girdhaari"]
+let godName = 0;
+while (godName < NamesOfGod.length) {
+    // console.log(`Krishna is ${NamesOfGod[godName]}`);
+    godName++;
+}
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`)
+    score = score+1;
+} while (score<=10);
+
+// Do while loop will minimum run for one time whether condition is met or not as
+// shown in the case above
